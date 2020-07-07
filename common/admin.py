2bbfe2536dc58e7c92e955af597ddb706461a83b
zcw <527713426@qq.com>
2020-07-07 16:26:35 +0900

    共通部品の修正

--- common/admin.py
+++ common/admin.py
@@ -7,21 +7,17 @@ from common.models import Coordinate, CodeConst
 
 # Register your models here.
 @admin.register(CodeConst)
-# class RecordAdmin(admin.ModelAdmin):
-# class RecordAdmin(ImportExportModelAdmin):
 class CodeConstAdmin(admin.ModelAdmin):
 
-    list_display = ('big_code', 'big_name', 'small_code', 'small_name')
+    list_display = ('cd', 'cdNm', 'subCd', 'subNm')
     list_per_page = 15
 
-    list_filter = ('big_code', 'big_name', 'small_code', 'small_name')
+    list_filter = ('cd', 'cdNm', 'subCd', 'subNm')
 
 
 # Register your models here.
 @admin.register(Coordinate)
-# class RecordAdmin(admin.ModelAdmin):
-# class RecordAdmin(ImportExportModelAdmin):
 class CoordinateAdmin(admin.ModelAdmin):
 
-    list_display = ('coorDivision', 'dspOrder', 'coorName', 'coorX', 'coorY', 'fixedValue', 'remarks')
+    list_display = ('crdDiv', 'itemNm', 'itemSort', 'crdX', 'crdY', 'defVal', 'cmnt1')
     list_per_page = 15
--- common/models.py
+++ common/models.py
@@ -4,37 +4,75 @@ from django.db import models
 from common import const
 
 
-class CodeConst(models.Model):
-    big_code = models.CharField(verbose_name='大分類コード', max_length=20)
-    big_name = models.CharField(verbose_name='大分類名', max_length=20)
-    small_code = models.CharField(verbose_name='小分類コード', max_length=20)
-    small_name = models.CharField(verbose_name='小分類名', max_length=20)
+# コードマスタ
+class CodeMst(models.Model):
+    # 大分類コード
+    cd = models.CharField(max_length=3, verbose_name="大分類コード", help_text='数字で３桁入力してください.',
+                          null=False, blank=False, db_index=True)
+    # 大分類名
+    cdNm = models.CharField(max_length=20, verbose_name="大分類名", help_text='20文字まで入力してください.',
+                               null=False, blank=False)
+    # 小分類コード
+    subCd = models.CharField(max_length=3, verbose_name="小分類コード", help_text='数字で３桁まで入力してください.',
+                              null=False, blank=False)
+    # 小分類名
+    subNm = models.CharField(max_length=20, verbose_name="小分類名", help_text='20文字まで入力してください.',
+                                   null=False, blank=False)
+    # 適用状態
+    delFlg_choices = (
+        (Const.DEL_FLG_0, '有効'),
+        (Const.DEL_FLG_1, '無効'),
+    )
+    delFlg = models.CharField(verbose_name="適用状態", choices=delFlg_choices, max_length=1, default=Const.DEF_DEL_FLG)
+    # 登録日付
+    createTime = models.DateTimeField(verbose_name='登録日付', auto_now=True)
+    # 更新日付
+    updateTime = models.DateTimeField(verbose_name='更新日付', auto_now=True)
 
     class Meta:
-        verbose_name = "コード管理"
+        verbose_name = "コード"
         verbose_name_plural = "コード管理"
 
     def __str__(self):
-        return self.big_name
+        return self.cd
 
 
-class Coordinate(models.Model):
-    coorDivision_choices = CodeConst.objects.filter(big_code=const.EXCEL_COORDINATE).\
-        values_list('small_code', 'small_name').order_by('small_code')
-    coorDivision = models.CharField(max_length=3, verbose_name='座標区分', default='0', choices=coorDivision_choices)
-    dspOrder = models.IntegerField(verbose_name='表示順')
-    coorName = models.CharField(verbose_name='座標名', max_length=20)
-    coorX = models.IntegerField(verbose_name='X座標')
-    coorY = models.IntegerField(verbose_name='Y座標')
-    fixedValue = models.CharField(verbose_name='固定値', max_length=20, null=True, blank=True)
-    remarks = models.CharField(verbose_name='備考', max_length=20, null=True, blank=True)
+# 座標マスタ
+class CrdMst(models.Model):
+    # 座標分類
+    crdDiv_choices = CodeMst.objects.filter(cd=Const.CRD_DIV_CD, del_flg=Const.DEL_FLG_0). \
+        values_list('subCd', 'subNm').order_by('subCd')
+    crdDiv = models.CharField(verbose_name="座標分類", choices=crdDiv_choices, max_length=3,
+                              null=False, blank=False, db_index=True, default=Const.CRD_DIV_H)
+    # 項目名
+    itemNm = models.CharField(verbose_name="項目名", max_length=60, null=False, blank=False)
+    # 項目順
+    itemSort = models.IntegerField(verbose_name="項目順", null=False, blank=False)
+    # 横座標
+    crdX = models.IntegerField(verbose_name="横座標", null=False, blank=False)
+    # 縦座標
+    crdY = models.IntegerField(verbose_name="縦座標", null=False, blank=False)
+    # 固定文言
+    defVal = models.CharField(verbose_name="固定文言", max_length=60, help_text='固定文言を設定してください.',
+                              null=True, blank=True)
+    # 備考文言1
+    cmnt1 = models.TextField(verbose_name="備考1", max_length=60, help_text='60文字まで入力してください.',
+                             null=True, blank=True)
+    # 備考文言2
+    cmnt2 = models.TextField(verbose_name="備考2", max_length=60, help_text='60文字まで入力してください.',
+                             null=True, blank=True)
+    # 適用状態
+    delFlg_choices = CodeMst.objects.filter(cd=Const.DEL_FLG_CD, del_flg=Const.DEL_FLG_0).\
+        values_list('subCd', 'subNm').order_by('subCd')
+    delFlg = models.CharField(verbose_name="適用状態", choices=delFlg_choices, max_length=1, default=Const.DEF_DEL_FLG)
+    # 登録日付
+    createTime = models.DateTimeField(verbose_name='登録日付', auto_now=True)
+    # 更新日付
+    updateTime = models.DateTimeField(verbose_name='更新日付', auto_now=True)
 
     class Meta:
-        verbose_name = "Excel座標管理"
-        verbose_name_plural = "Excel座標管理"
+        verbose_name = "座標"
+        verbose_name_plural = "座標管理"
 
     def __str__(self):
-        return self.coorName
-
-
-
+        return self.crdDiv
