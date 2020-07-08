4bd541f914445c230c718a1506b08d7fa08e7392
zzyu123 <2927143147@qq.com>
2020-07-08 12:19:59 +0900

    立替金model admin提出

--- company/admin.py
+++ company/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import get_permission_codename
 from django.contrib import admin, messages
-from company.models import Apply, Detail, Employee
+from django_filters import DateFilter
+
+from common.const import const
+from company.models import Apply, Detail, Employee, Paysub, Paymain
 
 
 class DetailInline(admin.TabularInline):
@@ -104,8 +107,7 @@ class ApplyAdmin(admin.ModelAdmin):
 
 @admin.register(Employee)
 class Employee(admin.ModelAdmin):
-    fieldsets = [(None, {
-        'fields': ['name', 'empNo', 'gender', 'birthday', 'email', 'zipCode', 'homeAddr', 'phone', 'user', 'empSts']})]
+    fieldsets = [(None, {'fields': ['name', 'empNo', 'gender', 'birthday', 'email', 'zipCode', 'homeAddr', 'phone', 'user', 'empSts']})]
     list_display = ('name', 'empNo', 'gender', 'birthday', 'email', 'zipCode', 'homeAddr', 'phone', 'empSts')
     search_fields = ('name', 'empNo')
     list_per_page = 20
@@ -113,3 +115,79 @@ class Employee(admin.ModelAdmin):
     list_filter = ('name', 'empNo', 'empSts')
 
     list_display_links = ('name',)
+
+# 立替金admin
+class PaysubInline(admin.TabularInline):
+    model = Paysub
+    extra = 1  # 默认显示条目的数量
+    fieldsets = [(None, {'fields': ['usedate',  'komoku', 'detail_text', 'price']})]
+
+@admin.register(Paymain)
+class PaymainAdmin(admin.ModelAdmin):
+    inlines = [PaysubInline, ]
+    fieldsets = [(None, {'fields': ['applydate', 'bikou_text']})]
+    list_display = ('applyer', 'applydate', 'total_money', 'bikou_text', 'status')
+    list_filter = ('applyer', 'status', ('applydate', DateFilter))
+    list_per_page = 10
+
+    actions = ['tichu_button', 'chengren_button', 'cancel_button']
+    def save_model(self, request, obj, form, change):
+        obj.user_id = request.user.id
+        obj.applyer = Employee.objects.get(user_id=request.user.id).name
+        subquery = Paysub.objects.filter(paymain_id=obj.id)
+        obj.total_money = 0
+        for line in subquery:
+            obj.total_money = obj.total_money + line.price
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user_id=request.user.id)
+
+    def tichu_button(self, request, queryset):
+        for obj in queryset:
+            if obj.status != const.WORK_TYPE_SMALL_0:
+                messages.add_message(request, messages.ERROR, '未提出を選んでください！')
+                return
+        queryset.update(status=const.WORK_TYPE_SMALL_1)
+
+    # 显示的文本，与django admin一致
+    tichu_button.short_description = ' 提出'
+    # icon，参考element-ui icon与https://fontawesome.com
+    tichu_button.icon = 'fas fa-check-circle'
+    # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    tichu_button.type = 'success'
+     # 承認
+    def chengren_button(self, request, queryset):
+        for obj in queryset:
+            if obj.status != const.WORK_TYPE_SMALL_1:
+                messages.add_message(request, messages.ERROR, '提出済を選んでください！')
+                return
+        queryset.update(status=const.WORK_TYPE_SMALL_2)
+
+    # 显示的文本，与django admin一致
+    chengren_button.short_description = ' 承認'
+    # icon，参考element-ui icon与https://fontawesome.com
+    chengren_button.icon = 'fas fa-check-circle'
+    # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    chengren_button.type = 'success'
+    chengren_button.allowed_permissions = ('chengren_button',)
+
+    #承認権限チェック
+    def has_chengren_button_permission(self, request):
+        if not request.user.is_superuser:
+            return False
+        opts = self.opts
+        codename = get_permission_codename('chengren_button', opts)
+        return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+    #取消
+    def cancel_button(self, request, queryset):
+        queryset.update(status=const.WORK_TYPE_SMALL_0)
+    # 显示的文本，与django admin一致
+    cancel_button.short_description = ' 取消'
+    # icon，参考element-ui icon与https://fontawesome.com
+    cancel_button.icon = 'fas fa-check-circle'
+    # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    cancel_button.type = 'warning'
