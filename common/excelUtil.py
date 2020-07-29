42ce720ba07282fad8f84f5e15c7253976cae6f2
YU <yubq20170214@gmail.com>
2020-07-29 11:59:47 +0900

    月度単位の集計表modelについての追加と変更

--- common/excelUtil.py
+++ common/excelUtil.py
@@ -61,12 +61,12 @@ def mkExcel(queryset, folder_name):
     sheet = book.get_sheet_by_name(const.SHEET_MONTH)
 
     # 月度単位の集計表ヘッダー部の取得
-    headMst = CrdMst.objects.filter(crdDiv=const.CRD_DIV_H, delFlg=const.DEL_FLG_0).\
+    headMst = CrdMst.objects.filter(tplType=const.AGG_MONTH, crdDiv=const.CRD_DIV_H, delFlg=const.DEL_FLG_0).\
     values_list('crdY', 'crdX', 'defVal').order_by('itemSort')
     # 月度単位の集計表明細部の取得
-    dataMst = CrdMst.objects.filter(crdDiv=const.CRD_DIV_D, delFlg=const.DEL_FLG_0).\
+    dataMst = CrdMst.objects.filter(tplType=const.AGG_MONTH, crdDiv=const.CRD_DIV_D, delFlg=const.DEL_FLG_0).\
         values_list('crdY', 'crdX', 'defVal').order_by('itemSort')
-    data_row = dataMst[6][0]
+    data_row = dataMst[0][0]
     i = 3
     for obj in queryset:
         data_row = data_row + 1
@@ -90,23 +90,23 @@ def mkExcel(queryset, folder_name):
         objMonth = obj.attendance_YM.strftime('%Y-%m')
 
         # 社員番号
-        sheet.cell(data_row, dataMst[6][1], aggEmpNo)
+        sheet.cell(data_row, dataMst[0][1], aggEmpNo)
         # 社員名前
-        sheet.cell(data_row, dataMst[7][1], aggName)
+        sheet.cell(data_row, dataMst[1][1], aggName)
         # 実働時間
-        sheet.cell(data_row, dataMst[8][1], aggWorkTime)
+        sheet.cell(data_row, dataMst[2][1], aggWorkTime)
         # 出勤日数
-        sheet.cell(data_row, dataMst[9][1], aggAttendCount)
+        sheet.cell(data_row, dataMst[3][1], aggAttendCount)
         # 欠勤日数
-        sheet.cell(data_row, dataMst[10][1], aggAbsCount)
+        sheet.cell(data_row, dataMst[4][1], aggAbsCount)
         # 年休日数
-        sheet.cell(data_row, dataMst[11][1], aggAnnLeave)
+        sheet.cell(data_row, dataMst[5][1], aggAnnLeave)
         # 休出日数
-        sheet.cell(data_row, dataMst[12][1], aggRestCount)
+        sheet.cell(data_row, dataMst[6][1], aggRestCount)
         # 遅早退日数
-        sheet.cell(data_row, dataMst[13][1], aggLateCount)
+        sheet.cell(data_row, dataMst[7][1], aggLateCount)
         # 統計年月
-        sheet.cell(headMst[6][0], headMst[6][1], str(objMonth))
+        sheet.cell(headMst[0][0], headMst[0][1], str(objMonth))
         # 行ごとに書式のコピー
         i = i + 1
         for a in range(1, 9):
