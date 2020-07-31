24eb2ffaa8f5bc162127163548d7adf8a3e3fdf9
zzyu123 <2927143147@qq.com>
2020-07-31 15:57:22 +0900

    勤務統計js添加

---
+++ static/attendancestatistics/js/attendancestatistics.js
@@ -0,0 +1,21 @@
+(function () {
+
+    // 加载事件
+    window.addEventListener('load', function (e) {
+
+        // 一覧明細
+        var changeList = $("tr[class^='row']");
+        // 編集リンク削除・追加
+        if (changeList) {
+            for (i = 0; i < changeList.length; i++) {
+                    // 社員番号
+                    var empNo = $(changeList[i]).find("th[class='field-empNo']").text()
+                    // 社員番号編集リンクを削除する
+                    $(changeList[i]).find("th[class='field-empNo']").find('a').remove();
+                    // もとの表示内容を追加
+                    $(changeList[i]).find("th[class='field-empNo']").html(empNo);
+             }
+        }
+
+    });
+})();
