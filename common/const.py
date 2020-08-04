bfd1dd8c283009091510f1ae00380947abf9fd5b
zzyu123 <2927143147@qq.com>
2020-08-04 11:56:47 +0900

    性别デフォルト修改

--- common/const.py
+++ common/const.py
@@ -68,7 +68,7 @@ const.MAIL_KBN_COMMIT = '1'
 const.MAIL_KBN_CANCEL = '2'
 const.MAIL_KBN_CONFIRM = '3'
 # 性别デフォルト
-const.GENDER_DEF = '1'
+const.GENDER_DEF = '0'
 const.EMPLOYEE_DEF = '0'
 
 # 作業状態小分類：0 未提出; 1 提出済; 2 承認済
