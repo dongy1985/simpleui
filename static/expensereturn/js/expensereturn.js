ab3d6c4385cbcd4d964cc224203ed0ac49ff6200
zzyu123 <2927143147@qq.com>
2020-08-05 14:24:25 +0900

    立替金js修改

--- static/expensereturn/js/expensereturn.js
+++ static/expensereturn/js/expensereturn.js
@@ -8,24 +8,24 @@
         // 編集リンク削除・追加
         if (changeList) {
             for (i = 0; i < changeList.length; i++) {
-                // 申请者名前
-                var duty_name = $(changeList[i]).find("th[class='field-applyer']").text()
-                // 申请状態
-                var duty_sts = $(changeList[i]).find("td[class='field-status']").text()
+                // 社員名前
+                var name = $(changeList[i]).find("th[class='field-name']").text()
+                // 報告状態
+                var status = $(changeList[i]).find("td[class='field-status']").text()
 
-                // 申请状態が提出済または承認済の場合、編集リンクを削除する
-                if (duty_sts == '提出済' || duty_sts == '承認済'){
+                // 報告状態が提出済または承認済の場合、編集リンクを削除する
+                if (status == '提出済' || status == '承認済'){
                     // リンク削除
-                    $(changeList[i]).find("th[class='field-applyer']").find('a').remove();
+                    $(changeList[i]).find("th[class='field-name']").find('a').remove();
                     // もとの表示内容を追加
-                    $(changeList[i]).find("th[class='field-applyer']").html(duty_name);
+                    $(changeList[i]).find("th[class='field-name']").html(name);
                 }
              }
         }
-        // 選択されている明細の申请状態より削除ボタンの活性化制御
+        // 選択されている明細の報告状態より削除ボタンの活性化制御
         var btnCtrlFunc = function() {
 
-            // ログインユーザがすスーパーユーザかを判断する
+            // ログインユーザがすスーパーユーザかを判断する(文字列　⇒　Boolean)
            var authPermission = eval($("#authPermission").val().toLowerCase());
 
             // 一覧明細
@@ -36,8 +36,9 @@
             var confirmBtn = $(".actions button[data-name='commit_button']");
             // 承認ボタン
             var enableBtn = $(".actions button[data-name='confirm_button']");
-            // 職員取消ボタン
+            // 取消ボタン
             var cancelBtn = $(".actions button[data-name='cancel_button']");
+
             // 削除ボタンを表示する
             if (delBtn) {
                 delBtn.show();
@@ -50,19 +51,20 @@
             if (enableBtn) {
                 enableBtn.show();
             }
-            // 職員取消ボタンを表示する
+            // 取消ボタンを表示する
             if (cancelBtn) {
                 cancelBtn.show();
             }
+
             if (changeList) {
                 for (i = 0; i < changeList.length; i++) {
                     // 選択されているかを判断する
                     var selectedFlg = $(changeList[i]).find('tr').context.classList.contains('selected');
-                    // 申请状態
+                    // 報告状態
                     var duty_sts = $(changeList[i]).find("td[class='field-status']").text()
 
-                    // 申请状態が提出済または承認済の場合、削除ボタンを隠す
-                    if (selectedFlg && (duty_sts == '提出済' || duty_sts == '承認済')){
+                    // 報告状態が提出済または承認済の場合、削除ボタンを隠す
+                    if (selectedFlg && (duty_sts == '提出済')){
                         // 削除ボタンを隠す
                         if (delBtn) {
                             delBtn.hide();
@@ -71,28 +73,34 @@
                         if (confirmBtn) {
                             confirmBtn.hide();
                         }
-                        if (selectedFlg && (duty_sts == '承認済')){
-                            // 承認ボタンを隠す
-                            if (enableBtn) {
-                                enableBtn.hide();
-                            }
-                        }
-                        break;
                     }
-                    if (selectedFlg && (duty_sts == '未提出')){
+                    if (selectedFlg && (duty_sts == '承認済')) {
+                        // 提出ボタンを隠す
+                        if (confirmBtn) {
+                            confirmBtn.hide();
+                        }
+                        // 削除ボタンを隠す
+                        if (delBtn) {
+                            delBtn.hide();
+                        }
                         // 承認ボタンを隠す
                         if (enableBtn) {
                             enableBtn.hide();
                         }
-                        // 職員取消ボタン
+                    }
+                    if (selectedFlg && (duty_sts == '未提出')) {
+                        // 承認ボタンを隠す
+                        if (enableBtn) {
+                            enableBtn.hide();
+                        }
+                        // 取消ボタンを隠す
                         if (cancelBtn) {
                             cancelBtn.hide();
                         }
-                        break;
                     }
                  }
             }
-      }
+        }
 
         // 明細一覧のチェックボックスのイベント
         $(".action-checkbox").bind('click', function(e){
