792665fa9a5a88c181af0514fc9250597a5eb99d
YU <yubq20170214@gmail.com>
2020-08-12 02:11:22 +0900

    js 立替金、通勤手当のコメント修正

--- static/dutyamount/js/dutydetail.js
+++ static/dutyamount/js/dutydetail.js
@@ -2,33 +2,33 @@
 
     // 加载事件
     window.addEventListener('load', function (e) {
-        // 保存ボタン
-        var saveBtn = $(".el-button--primary");
-        // 一覧明細
+       // 金額のフォーマット eg:9000→9,000
+       function formatter(num) {
+              reg = /(?=(\B)(\d{3})+$)/g;
+              var temp = num.replace(',','')
+              var ss = temp.replace(reg,',')
+              return ss
+        }
+        // 金額フォーマットの取消 eg:9,000→9000
+        function unFormatter(num) {
+              var uu = num.replace(',','')
+              return uu
+        }
+        // 通勤手当の一覧明細
         var changeList = $("tr[id^='dutydetail_set-']");
-        // 編集リンク削除・追加
         if (changeList) {
             for (i = 0; i < changeList.length; i++) {
+                // 金額テキストボックスのidを取得
                 var start="id_dutydetail_set-";
                 var end="-trafficAmount";
                 var duty_id = start + i + end;
-
-                function formatter(num) {
-                      reg = /(?=(\B)(\d{3})+$)/g;
-                      var temp = num.replace(',','')
-                      var ss = temp.replace(reg,',')
-                      return ss
-                }
-                function unFormatter(num) {
-                      var uu = num.replace(',','')
-                      return uu
-                }
-                  // テキストボックスにフォーカス時、フォームの背景色を変化
+                // 金額テキストボックスにフォーカス時、金額フォーマットの取消 eg:9,000→9000
                 $("#"+duty_id).focus(function() {
                     var unValue=$(this).val();
                     var uu = unFormatter(unValue);
                     $(this).val(uu);
                   });
+                // 金額テキストボックスにフォーカスアウト時、金額のフォーマット eg:9000→9,000
                 $("#"+duty_id).blur(function() {
                     var curValue=$(this).val();
                     var amtreg=/^([1-9]\d{0,9}|0)([.]?|(\.\d{1,2})?)$/;
@@ -44,29 +44,19 @@
         }
 
         var next = changeList.length-1;
-        // 明細一覧のチェックボックスのイベント
+        // 「通勤手当明細の追加」ボタン押下後のイベント
         $("a").click(function(){
+            // 金額テキストボックスのidを取得
             var start="id_dutydetail_set-";
             var end="-trafficAmount";
             var duty_nextId = start + next + end;
-
-            function formatter(num) {
-                  reg = /(?=(\B)(\d{3})+$)/g;
-                  var temp = num.replace(',','')
-                  var ss = temp.replace(reg,',')
-                  return ss
-            }
-            function unFormatter(num) {
-                  var uu = num.replace(',','')
-                  return uu
-            }
-              // テキストボックスにフォーカス時、フォームの背景色を変化
+            // 金額テキストボックスにフォーカス時、金額フォーマットの取消 eg:9,000→9000
             $("#"+duty_nextId).focus(function() {
                 var unValue=$(this).val();
                 var uu = unFormatter(unValue);
                 $(this).val(uu);
               });
-
+            // 金額テキストボックスにフォーカスアウト時、金額のフォーマット eg:9000→9,000
             $("#"+duty_nextId).blur(function() {
                 var curValue=$(this).val();
                 var amtreg=/^([1-9]\d{0,9}|0)([.]?|(\.\d{1,2})?)$/;
--- static/expensereturn/js/expensedetail.js
+++ static/expensereturn/js/expensedetail.js
@@ -2,33 +2,33 @@
 
     // 加载事件
     window.addEventListener('load', function (e) {
-
-        // 一覧明細
+        // 単一金額のフォーマット eg:9000→9,000
+        function formatter(num) {
+              reg = /(?=(\B)(\d{3})+$)/g;
+              var temp = num.replace(',','')
+              var ss = temp.replace(reg,',')
+              return ss
+        }
+        // 単一金額フォーマットの取消 eg:9,000→9000
+        function unFormatter(num) {
+              var uu = num.replace(',','')
+              return uu
+        }
+        // 項目明細の一覧明細
         var changeList = $("tr[id^='expensereturndetail_set-']");
-        // 編集リンク削除・追加
         if (changeList) {
             for (i = 0; i < changeList.length; i++) {
+                // 単一金額テキストボックスのidを取得
                 var start="id_expensereturndetail_set-";
                 var end="-price";
                 var expense_id = start + i + end;
-
-                function formatter(num) {
-                      reg = /(?=(\B)(\d{3})+$)/g;
-                      var temp = num.replace(',','')
-                      var ss = temp.replace(reg,',')
-                      return ss
-                }
-                function unFormatter(num) {
-                      var uu = num.replace(',','')
-                      return uu
-                }
-
-                  // テキストボックスにフォーカス時、フォームの背景色を変化
+                // 単一金額テキストボックスにフォーカス時、金額フォーマットの取消 eg:9,000→9000
                 $("#"+expense_id).focus(function() {
                     var unValue=$(this).val();
                     var uu = unFormatter(unValue);
                     $(this).val(uu);
                   });
+                // 単一金額テキストボックスにフォーカスアウト時、金額のフォーマット eg:9000→9,000
                 $("#"+expense_id).blur(function() {
                     var curValue=$(this).val();
                     var amtreg=/^([1-9]\d{0,9}|0)([.]?|(\.\d{1,2})?)$/;
@@ -44,29 +44,19 @@
         }
 
         var next = changeList.length-1;
-        // 明細一覧のチェックボックスのイベント
+        // 「項目明細の追加」ボタン押下後のイベント
         $("a").click(function(){
+            // 単一金額テキストボックスのidを取得
             var start="id_expensereturndetail_set-";
             var end="-price";
             var expense_nextId = start + next + end;
-
-            function formatter(num) {
-                  reg = /(?=(\B)(\d{3})+$)/g;
-                  var temp = num.replace(',','')
-                  var ss = temp.replace(reg,',')
-                  return ss
-            }
-            function unFormatter(num) {
-                  var uu = num.replace(',','')
-                  return uu
-            }
-              // テキストボックスにフォーカス時、フォームの背景色を変化
+            // 単一金額テキストボックスにフォーカス時、金額フォーマットの取消 eg:9,000→9000
             $("#"+expense_nextId).focus(function() {
                 var unValue=$(this).val();
                 var uu = unFormatter(unValue);
                 $(this).val(uu);
               });
-
+            // 単一金額テキストボックスにフォーカスアウト時、金額のフォーマット eg:9000→9,000
             $("#"+expense_nextId).blur(function() {
                 var curValue=$(this).val();
                 var amtreg=/^([1-9]\d{0,9}|0)([.]?|(\.\d{1,2})?)$/;
