578ab77a0ed415a7af033c82121eb0d9db9c78be
zzyu123 <2927143147@qq.com>
2020-09-17 11:17:33 +0900

    社員情報と資産情報adminの説明文添加

--- company/admin.py
+++ company/admin.py
@@ -18,13 +18,18 @@ from django.db.models import Q
 # 社員admin
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    # 社員情報フォーム導入
     form = EmployeeAdminForm
+    # 社員情報フォーム表示のfieldsets制御
     def get_form(self, request, obj=None, **kwargs):
         self.exclude = []
+        # スーパーユーザー或いは社員情報を添加する権限を持つユーザーはfieldsets全表示
         if request.user.is_superuser or request.user.has_perm('company.add_employee'):
             self.exclude = []
+        # 普通社員のログインユーザーは自分の社員情報を見られる
         elif obj.name == Employee.objects.get(user_id=request.user.id).name:
             self.exclude = []
+        # 普通社員のログインユーザーは自分の社員情報以外、他のユーザーの社員情報の名前、社員番号、メールしかを見えない
         else:
             self.exclude.extend(
                 ['gender', 'birthday', 'zipCode', 'homeAddr', 'phone', 'retention_code', 'retention_limit', 'user',
@@ -35,10 +40,12 @@ class EmployeeAdmin(admin.ModelAdmin):
         'fields': ['name', 'empNo', 'gender', 'birthday', 'email', 'zipCode', 'homeAddr', 'phone', 'retention_code',
                    'retention_limit', 'user', 'empSts']})]
 
-    # 要显示的字段
+    # 一覧画面のfieldset表示
     def changelist_view(self, request, extra_context=None):
-        if request.user.is_superuser:
+        # スーパーユーザー或いは社員情報を添加する権限を持つユーザーはfieldsetsの社員情報の名前、社員番号、メールと携帯番号を見える
+        if request.user.is_superuser or request.user.has_perm('company.add_employee'):
             self.list_display = ['name', 'empNo', 'email', 'phone']
+        # 普通社員は社員情報の名前、社員番号、メールしかを見えない
         else:
             self.list_display = ('name', 'empNo', 'email',)
         return super(EmployeeAdmin, self).changelist_view(request=request, extra_context=None)
@@ -49,24 +56,31 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_filter = ('empSts',)
     ordering = ('-empNo',)
 
+    # 社員情報を添加する権限の制御
     def has_add_permission(self, request):
         opts = self.opts
         codename = get_permission_codename('add', opts)
+        # 社員情報を添加する権限を持つユーザーとスーパーユーザー以外、添加権限なし
         if request.user.has_perm('company.add_employee') == False and request.user.is_superuser == False:
             return False
         else:
             return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
+    # 権限別でreadonly制御
     def get_readonly_fields(self, request, obj=None):
+        # スーパーユーザー或いは社員情報を添加する権限を持つユーザーは、readonlyのfieldsがない
         if request.user.is_superuser or request.user.has_perm('company.add_employee'):
             self.readonly_fields = []
+        # 普通社員は社員情報の名前、社員番号、メールをreadonlyとして設定する
         else:
             self.readonly_fields = ['name', 'empNo', 'email']
         return self.readonly_fields
 
+    # readonly制御
     readonly_fields = ('name', 'empNo', 'email')
 
     def save_model(self, request, obj, form, change):
+        # 社員名が変更される場合、各モジュールの社員名更新
         if change:
             # 立替金
             expenRe = ExpenseReturn.objects.filter(user_id=obj.user_id)
@@ -90,6 +104,7 @@ class EmployeeAdmin(admin.ModelAdmin):
                 dutySta.update(name=obj.name)
         super().save_model(request, obj, form, change)
 
+    # get_formメソッドと組み合わせとしてfieldsets獲得する
     def get_fieldsets(self, request, obj=None):
         return [(None, {'fields': self.get_fields(request, obj)})]
 
@@ -116,10 +131,13 @@ class AssetManageAdmin(admin.ModelAdmin):
     actions_on_top = True
 
     def save_model(self, request, obj, form, change):
+        # 資産情報変更される場合、資産貸出申請の情報を更新する
         if change:
             query = AssetLend.objects.filter(asset_id=obj.id)
+            # 該当資産番号を持つ資産貸出申請記録が存在する場合
             if query.count() != 0:
                 subNm = CodeMst.objects.get(cd=const.ASSET_TYPE, subCd=obj.type).subNm
+                # 資産番号、分類、資産名を更新する
                 query.update(asset_code=obj.asset, type=subNm, name=obj.name)
         super().save_model(request, obj, form, change)
 
